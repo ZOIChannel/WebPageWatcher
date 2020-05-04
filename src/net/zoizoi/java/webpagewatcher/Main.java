00f0b4149e24b957614f712754ba63971968fec3
ZOI <zoizoichannel@gmail.com>
2020-05-05 02:07:17 +0900

    表などの関数追加

--- src/net/zoizoi/java/webpagewatcher/Main.java
+++ src/net/zoizoi/java/webpagewatcher/Main.java
@@ -1,8 +1,36 @@
 package net.zoizoi.java.webpagewatcher;
 
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.ArrayList;
+import java.util.List;
+
 public class Main {
 
-    public static void main(String[] args) {
-	// write your code here
+
+  public static void main(String[] args) {
+    Boolean Running = true;
+    List<Integer> CodeList = new ArrayList<Integer>();
+    List<String> URLList = new ArrayList<String>();
+    while (Running) {
     }
+  }
+
+  public static String GetURL() throws IOException {
+    BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
+    System.out.println("Please Write URL...");
+    String url = reader.readLine();
+    System.out.println("Start watching " + url + " ...");
+    return url;
+  }
+
+  public static boolean WriteTable(int Code, String URL) {
+    String Line_Row = " +-----+---------------";
+    String Line_Column = " | ";
+    System.out.println(Line_Row);
+    System.out.println(Line_Column + Code + Line_Column + URL);
+    System.out.println(Line_Row);
+    return true;
+  }
 }
